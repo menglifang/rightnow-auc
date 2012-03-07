aabc40162beb397951a67aa4e25636506d6afc0e
Tower He <towerhe@gmail.com>
2012-03-07 16:00:44 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RightnowAuc::Application.config.secret_token = '1d663f391b0998a8ab3b5a1442515461b708b275b99b9faed4f836f7ad6dddf8db32656e8bac36b9a6ac31679a948d353a194e85d39d1dc2b20084749c8b0b26'
