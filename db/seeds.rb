e81d9deb86d244da1b229a2762dc413c1949446a
Tower He <towerhe@gmail.com>
2012-06-17 20:14:38 +0800

    Fixed password too short issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-User.create! email: 'admin@example.org', password: 'admin', password_confirmation: 'admin', role: :admin
+User.create! email: 'admin@example.org', password: '123456', password_confirmation: '123456', role: :admin
