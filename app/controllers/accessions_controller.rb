e659aca211f7933e0e1c3f4ba3f5b625a6706296
Tower He <towerhe@gmail.com>
2012-07-06 23:45:08 +0800

    Assigned @accessions instead of @accessible_applications

--- app/controllers/accessions_controller.rb
+++ app/controllers/accessions_controller.rb
@@ -1,9 +1,28 @@
 class AccessionsController < ApplicationController
   before_filter :authenticate_admin!
+  before_filter :load_user
 
   def index
-    user = User.find(params[:user_id])
-    @accessible_applications = user.applications
-    @inaccessible_applications = Doorkeeper::Application.all - user.applications
+    @accessions = @user.accessions
+    @inaccessible_applications = Doorkeeper::Application.all - @user.applications
+  end
+
+  def create
+    @user.accessions.create(application_id: params[:application_id])
+
+    redirect_to user_accessions_url(@user)
+  end
+
+  def destroy
+    accession = Accession.find(params[:id])
+    accession.destroy
+
+    redirect_to user_accessions_url(@user)
+  end
+
+  private
+
+  def load_user
+    @user = User.find(params[:user_id])
   end
 end
