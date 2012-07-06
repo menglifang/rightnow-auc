b90e6d37049307269fc4cf4d6bc5ffda5300d02e
Tower He <towerhe@gmail.com>
2012-07-06 23:44:27 +0800

    Set application_id accessible

--- app/models/accession.rb
+++ app/models/accession.rb
@@ -2,5 +2,5 @@ class Accession < ActiveRecord::Base
   belongs_to :application, class_name: 'Doorkeeper::Application'
   belongs_to :user
 
-  attr_accessible :admin, :application
+  attr_accessible :admin, :application, :application_id
 end
