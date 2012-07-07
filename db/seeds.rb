38422eb7b1cce7d091e8a4940702678d97307c6c
Tower He <towerhe@gmail.com>
2012-07-07 23:48:09 +0800

    Added a default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
 User.create! email: 'admin@example.org', password: '123456', password_confirmation: '123456', role: :admin
+User.create! email: 'user@example.org', password: '123456', password_confirmation: '123456', role: :user
